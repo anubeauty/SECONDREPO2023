d033120e255d41038aee18d5cb4d384ff362f5fd
anu <teacherapps187@gmail.com>
2023-03-13 14:59:47 -0400

    second program coming

---
+++ src/main/java/anu/mygit/SecondGitProgram.java
@@ -0,0 +1,13 @@
+package anu.mygit;
+
+public class SecondGitProgram {
+	
+	public static void main(String args[]) {
+		
+		   
+		  System.out.println("hello this is my second program!!!");
+		
+		 
+	}
+
+}
